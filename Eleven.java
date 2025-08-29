1a930056ab2777c2f08cf9817315c9b3abd7ec33
omkhente-Dev <khenteomprakash@gmail.com>
2025-08-29 13:14:40 +0530

    Fourth

---
+++ Eleven.java
@@ -0,0 +1,7 @@
+public class Eleven {
+ public static void main(String arg[]) {
+System.out.print("Age : 23");
+System.out.print("Class : 12th");
+System.out.print("Name : Omprakash");
+ }
+}
---
+++ Twelth.java
@@ -0,0 +1,7 @@
+public class Twelth {
+ public static void main(String log[]) {
+System.out.printIn("College Name : Shri Shivaji senior college");
+System.out.printIn("Department : Computer Science");
+System.out.printIn("Course : Bachelor of science");
+ }
+}
